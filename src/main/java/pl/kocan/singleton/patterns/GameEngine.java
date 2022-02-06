b972d78a64c10ad58d16e7f02e6e24c8aa7ecd8f
Paweł Kocan <pkocan@onet.pl>
2022-02-06 15:01:50 +0100

    Singleton Example - another solution -> private static GameEngine instance = new GameEngine();

--- src/main/java/pl/kocan/singleton/patterns/GameEngine.java
+++ src/main/java/pl/kocan/singleton/patterns/GameEngine.java
@@ -4,21 +4,12 @@ public class GameEngine {
 
     private int hp = 100;
     private String characterName = "";
-    private static GameEngine instance;
+    private static GameEngine instance = new GameEngine();
 
     public GameEngine() {
     }
 
     public static GameEngine getInstance() {
-        if (instance == null) {
-            synchronized (GameEngine.class) {
-
-                if (instance == null) {
-                    instance = new GameEngine();
-                }
-
-            }
-        }
         return instance;
     }
 
